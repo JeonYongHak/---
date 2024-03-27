fe836577cbd731a4d5bdfa98c7073cd6bdea0e37
Abel Beak <abel@officener.kr>
2024-03-27 19:49:41 +0900

    Headless UI 설치

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,10 +5,39 @@ const config: Config = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/views/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        ow: '0px',
+        sm: '0px',
+        md: '744px',
+        lg: '1280px',
+        xl: '1280px',
+        '2xl': '1280px',
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '1rem',
+        md: '3rem',
+        lg: '5rem',
+        xl: '5rem',
+        '2xl': '5rem',
+      },
+      screens: {
+        ow: '744px',
+        sm: '744px',
+        md: '1280px',
+        lg: '1280px',
+        xl: '1280px',
+        '2xl': '1280px',
+      },
+    },
   },
-  plugins: [],
+  plugins: [require('@headlessui/tailwindcss')],
 };
 export default config;
