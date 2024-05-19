b05213bfd03f458c29300057b0a7e2dd8caa6aa0
Abel Beak <abel@officener.kr>
2024-05-19 15:44:58 +0900

    version update

--- src/app/terms/layout.tsx
+++ src/app/terms/layout.tsx
@@ -8,7 +8,7 @@ export const dynamic = 'force-static';
 
 export default function TermsLayout({ children }: Props) {
   return (
-    <main className="container prose-sm py-10 prose-th:border prose-th:py-2 prose-td:border prose-td:px-2">
+    <main className="prose-sm prose-th:border prose-th:py-2 prose-td:border prose-td:px-2 container py-10">
       {children}
     </main>
   );
