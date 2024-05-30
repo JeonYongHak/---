93986e021f3399062618deda4545ac4e894b321f
Abel Beak <abel@officener.kr>
2024-05-30 23:30:41 +0900

    middleware 잠시 비활성화 조치

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,15 +5,15 @@ export const config = {
 };
 
 export default async function middleware(req: NextRequest) {
-  const url = req.nextUrl;
-  const hostname = req.headers.get('host')!.replace('.localhost:3000', `.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`);
-  const searchParams = req.nextUrl.searchParams.toString();
-  const path = `${url.pathname}${searchParams.length > 0 ? `?${searchParams}` : ''}`;
-  const subDomain = hostname.split('.')[0];
-  switch (true) {
-    case subDomain !== hostname:
-      return NextResponse.rewrite(new URL(`/private/${subDomain}${path}`, req.url));
-    default:
-      return NextResponse.rewrite(new URL(`${path}`, req.url));
-  }
+  // const url = req.nextUrl;
+  // const hostname = req.headers.get('host')!.replace('.localhost:3000', `.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`);
+  // const searchParams = req.nextUrl.searchParams.toString();
+  // const path = `${url.pathname}${searchParams.length > 0 ? `?${searchParams}` : ''}`;
+  // const subDomain = hostname.split('.')[0];
+  // switch (true) {
+  //   case subDomain !== hostname:
+  //     return NextResponse.rewrite(new URL(`/private/${subDomain}${path}`, req.url));
+  //   default:
+  //     return NextResponse.rewrite(new URL(`${path}`, req.url));
+  // }
 }
